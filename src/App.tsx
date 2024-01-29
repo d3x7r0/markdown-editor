8e1ad7fa0fc377e2563da176c24b63177394dd85
Luis Nabais <dextro@gmail.com>
2024-01-29 09:15:54 +0000

    Style tweaks

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,11 @@ import './App.css'
 
 function App() {
   return (
-    <>
+    <div className="container px-4 mx-auto">
       <h1 className="text-3xl font-bold underline">
         Markdown Editor
       </h1>
-    </>
+    </div>
   )
 }
 
